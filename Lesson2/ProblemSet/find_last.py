9a05f109ca4e9dddc4b9b651bfe5c677721d5c60
xala3pa <salazar3pa@gmail.com>
2014-05-31 12:38:38 +0200

    find last solved

---
+++ Lesson2/ProblemSet/find_last.py
@@ -0,0 +1,44 @@
+# Define a procedure, find_last, that takes as input
+# two strings, a search string and a target string,
+# and returns the last position in the search string
+# where the target string appears, or -1 if there
+# are no occurences.
+#
+# Example: find_last('aaaa', 'a') returns 3
+
+# Make sure your procedure has a return statement.
+
+def find_last(text, target):
+    last_pos = -1 
+    while (True):
+        offset = text.find(target,last_pos + 1)
+        if (offset == -1):
+           return last_pos 
+        last_pos = offset
+
+print find_last('aaaa', 'a')
+#>>> 3
+
+print find_last('aaaaa', 'aa')
+#>>> 3
+
+print find_last('aaaa', 'b')
+#>>> -1
+
+print find_last("111111111", "1")
+#>>> 8
+
+print find_last("222222222", "")
+#>>> 9
+
+print find_last("", "3")
+#>>> -1
+
+print find_last("", "")
+#>>> 0
+
+
+
+
+
+
