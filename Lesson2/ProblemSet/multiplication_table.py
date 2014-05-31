6d49b88cdae25f6d45b4072fc03089214d97e56a
xala3pa <salazar3pa@gmail.com>
2014-05-31 13:07:17 +0200

    Multiplication table solved

---
+++ Lesson2/ProblemSet/multiplication_table.py
@@ -0,0 +1,43 @@
+# 2 GOLD STARS
+
+# Define a procedure, print_multiplication_table,
+# that takes as input a positive whole number, and prints out a multiplication,
+# table showing all the whole number multiplications up to and including the
+# input number. The order in which the equations are printed matters.
+
+# Hint: You can not add an integer and a string, but in homework 1.9
+# you came across a method, str, for turning an integer into a string.
+
+def print_multiplication_table(n):
+    t = 1
+    while n >= t:
+        i = 1
+        while i <= n:
+            print str(t) + " * " + str(i) + " = " + str(t*i) 
+            i = i + 1
+        t = t + 1
+
+
+
+
+
+
+#print_multiplication_table(2)
+#>>> 1 * 1 = 1
+#>>> 1 * 2 = 2
+#>>> 2 * 1 = 2
+#>>> 2 * 2 = 4
+
+print_multiplication_table(3)
+#>>> 1 * 1 = 1
+#>>> 1 * 2 = 2
+#>>> 1 * 3 = 3
+#>>> 2 * 1 = 2
+#>>> 2 * 2 = 4
+#>>> 2 * 3 = 6
+#>>> 3 * 1 = 3
+#>>> 3 * 2 = 6
+#>>> 3 * 3 = 9
+
+
+
