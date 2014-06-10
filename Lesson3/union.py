0d8334aff4d073e16a10e8844bc7d0e4ed8e657b
xala3pa <salazar3pa@gmail.com>
2014-06-10 19:08:36 +0200

    union solved

---
+++ Lesson3/union.py
@@ -0,0 +1,28 @@
+# Define a procedure, union,
+# that takes as inputs two lists.
+# It should modify the first input
+# list to be the set union of the two
+# lists. You may assume the first list
+# is a set, that is, it contains no 
+# repeated elements.
+
+def union(l1,l2):
+    for e in l2:
+        if e not in l1:
+            l1.append(e)
+    return l1
+    
+
+
+
+
+# To test, uncomment all lines 
+# below except those beginning with >>>.
+
+a = [1,2,3]
+b = [2,4,6]
+union(a,b)
+print a 
+#>>> [1,2,3,4,6]
+print b
+#>>> [2,4,6]
