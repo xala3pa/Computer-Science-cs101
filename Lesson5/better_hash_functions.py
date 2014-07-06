3afe9461a34fb86a4e0fc31cec2a4c741a95c097
xala3pa <salazar3pa@gmail.com>
2014-07-06 10:59:26 +0200

    better hash function solved

---
+++ Lesson5/better_hash_functions.py
@@ -0,0 +1,28 @@
+# Define a function, hash_string,
+# that takes as inputs a keyword
+# (string) and a number of buckets,
+# and returns a number representing
+# the bucket for that keyword.
+
+def hash_string(keyword,buckets):
+    if len(keyword) > 0:
+        total = 0
+        for w in keyword:
+            total += ord(w)
+    return total % buckets
+
+print hash_string('a',12)
+#>>> 1
+
+print hash_string('b',12)
+#>>> 2
+
+print hash_string('a',13)
+#>>> 6
+
+print hash_string('au',12)
+#>>> 10
+
+print hash_string('udacity',12)
+#>>> 11
+
