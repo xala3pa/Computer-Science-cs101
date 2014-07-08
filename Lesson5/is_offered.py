1090cb3c56b3ca2168262c66e17187ccb849e3de
xala3pa <salazar3pa@gmail.com>
2014-07-08 20:23:21 +0200

    is offered solved

---
+++ Lesson5/is_offered.py
@@ -0,0 +1,10 @@
+" Press ? for help
+
+.. (up a dir)
+<omputer-Science-cs101/Lesson5/
+  adding_keyword.py
+  better_hash_functions.py
+  empty_hash_table.py
+  finding_buckets.py
+  lookup.py
+  update.py
