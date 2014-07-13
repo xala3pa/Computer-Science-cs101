58f8f880e6bf17cef9b931bac1372e9f8c3b3205
xala3pa <salazar3pa@gmail.com>
2014-07-13 10:26:43 +0200

    shift a letter solved

--- Lesson5/shift_a_letter.py
+++ Lesson5/shift_a_letter.py
@@ -1,4 +1,13 @@
+# Write a procedure, shift, which takes as its input a lowercase letter,
+# a-z and returns the next letter in the alphabet after it, with 'a' 
+# following 'z'.
+
 def shift(letter):
     return chr( 97 + ( ( (ord(letter) + 1 - 97) % 26) ) )
 
+print shift('a')
+#>>> b
+print shift('n')
+#>>> o
 print shift('z')
+#>>> a
